11a8bc495a5ba552225f4ca0b9485d3fa01707bd
alex <38814044+alextwothousand@users.noreply.github.com>
2024-02-13 19:08:09 +0000

    Update gen_y.sh

--- gen_y.sh
+++ gen_y.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 echo "\nResHelper: Generating data...\n";
 name="shaper_calibrate_y";
-[ ! -d $HOME"/printer_data/config/RES_DATA/" ] && mkdir ~/printer_data/config/RES_DATA;
+[ ! -d $HOME"/klipper_config/RES_DATA/" ] && mkdir ~/klipper_config/RES_DATA;
 
 #graph generation
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o ~/printer_data/config/RES_DATA/shaper_calibrate_y.png;
+~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o ~/klipper_config/RES_DATA/shaper_calibrate_y.png;
 
 #damping ratio
 dr="$(Rscript ~/ResHelper/Y.R)";
@@ -14,5 +14,5 @@ name="$name-dr_$dr-v$(date "+%Y%m%d_%H%M").png";
 
 
 #cleanup
-mv ~/printer_data/config/RES_DATA/shaper_calibrate_y.png ~/printer_data/config/RES_DATA/$name;
+mv ~/klipper_config/RES_DATA/shaper_calibrate_y.png ~/klipper_config/RES_DATA/$name;
 find '/tmp/' -name "resonances_y_*.csv" -print 2>/dev/null -exec rm {} \;

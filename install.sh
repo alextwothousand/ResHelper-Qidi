77604932575895030b0a69d74c42d68335acd67a
alex <38814044+alextwothousand@users.noreply.github.com>
2024-02-13 19:09:12 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
-PD_DIR="${HOME}/printer_data/config"
+PD_DIR="${HOME}/klipper_config"
 
 echo -e "\nResHelper Installer\n"
 
 check_printdata()
 {
     if [ ! -d "${PD_DIR}" ]; then
-        echo -e "printer_data folder not found. Exiting ..."
+        echo -e "klipper_config folder not found. Exiting ..."
         exit -1
     else
-        echo "printer_data folder detected successfully!"
+        echo "klipper_config folder detected successfully!"
     fi
 
 }
 
 check_printdata
 
-if [ -w "${HOME}/printer_data/config/reshelper.cfg" ]; then                                                                                               
+if [ -w "${HOME}/klipper_config/reshelper.cfg" ]; then                                                                                               
 	 echo -e "\n Overwrite reshelper.cfg? y/n "
 	 read ANSWER
 	 case $ANSWER in
-  		 [yY]) echo -e "\n Overwriting .."; cp ~/ResHelper/reshelper.cfg ~/printer_data/config/reshelper.cfg;;
+  		 [yY]) echo -e "\n Overwriting .."; cp ~/ResHelper/reshelper.cfg ~/klipper_config/reshelper.cfg;;
  		 [nN]) echo -e "\n Skipping..";;
 	esac 
 else
 	echo -e "\n Copying configuration file to config folder...";
-	cp ~/ResHelper/reshelper.cfg ~/printer_data/config/reshelper.cfg;
+	cp ~/ResHelper/reshelper.cfg ~/klipper_config/reshelper.cfg;
 fi
 
 echo -e "\nCleaning old tmp csv files..."
